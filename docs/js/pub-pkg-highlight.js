39ce78a285586c73f0b38264822b5293c993c00c
jldec <jurgen@jldec.me>
2024-09-03 03:59:59 -0400

    bump deps

--- docs/js/pub-pkg-highlight.js
+++ docs/js/pub-pkg-highlight.js
@@ -4,7 +4,7 @@
  * browserify entry point - mainly to initialize highlight.js
  * also supports pub-generator 'update-view' events
  *
- * Copyright (c) 2015-2022 Jürgen Leschner - github.com/jldec - MIT license
+ * Copyright (c) 2015-2024 Jürgen Leschner - github.com/jldec - MIT license
  *
  * for highlight.js please see included static/js/LICENSE-HIGHLIGHT
  * or visit https://github.com/highlightjs/highlight.js
